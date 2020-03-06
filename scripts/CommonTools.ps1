b6b32d7d4916b65037a8b0665b06f1224c07f162
Jasper de Lange <jasper@de-lange.net>
2020-03-06 12:12:31 +0100

    add CommonTools script

---
+++ scripts/CommonTools.ps1
@@ -0,0 +1,8 @@
+choco install -y powertoys
+choco install -y 7-taskbar-tweaker
+choco install -y 7zip.install
+choco install -y teracopy
+choco install -y notion
+choco install -y paint.net
+choco install -y signal --package-parameters="'/NoShortcut'"
+#tresorit
